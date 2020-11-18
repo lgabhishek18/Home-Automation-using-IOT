3eabb644982ec16d1b4b387f0d8117ef77aeaacc
Abhishek Gupta <60277134+lgabhishek18@users.noreply.github.com>
2020-11-19 01:28:08 +0530

    code

---
+++ HomeAutomation.ino
@@ -0,0 +1,26 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "rBpGN7kIBtqazfDW09pI2gBiuqXt0FxR";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "your wifi name";
+char pass[] = "your password";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
